009f1ffe82636640d95f1b050b69483e0eeae8de
Umer <umerfarooqaman@outlook.com>
2019-04-08 11:25:23 +0500

    npm loadash

--- array.js
+++ array.js
@@ -1,5 +1,7 @@
 
+const loadash=require("loadash");
 
+//practice data
 
 
 var videos=[{id:3,name:"islamic",uid:12},{id:2,name:"music",uid:11},{id:1,name:"documentary",uid:10}];
@@ -9,27 +11,21 @@ var reported=[{uid:10,vid:3},{uid:10,vid:1},{uid:12,vid:1}];
 var users=[{id:1,name:"aman"},{id:2,name:"farooq"},{id:3,name:"usama"}];
 
 
+//initial data
 
-"use strict"
-
+console.log("---------------------videos----------------------------");
 console.log(videos);
-
-console.log("------------------");
-
+console.log("---------------------reported videos-------------------");
 console.log(reported);
-
-
-console.log("------------------");
-
+console.log("---------------------users-----------------------------");
 console.log(users);
-
-console.log("------------------");
+console.log("\n-------------------------------------------------------\n");
 
 
 
 
 //extract array if ids from video
-console.log("maping an array from members");
+console.log("maping an array of Ids from members of {id,name}\n");
 var res;
 
 res=videos.map(function (params) {
@@ -37,13 +33,13 @@ res=videos.map(function (params) {
 });
 console.log(res);
 
+console.log("\n-------------------------------------------------------\n");
 
 
 //extratct reported video by each users
 
-var merged=merge(videos,users);
 
-console.log(merged);
+
 
 //extract all videos which are not reported by users
 
