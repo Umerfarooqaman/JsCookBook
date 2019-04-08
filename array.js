1f2c11dad6f544308db3f8bce41c2761a4f13bca
Umer <umerfarooqaman@outlook.com>
2019-04-08 11:43:09 +0500

    difference with practeced

--- array.js
+++ array.js
@@ -1,14 +1,43 @@
-
-const loadash=require("loadash");
+const lodash = require("lodash")
 
 //practice data
 
 
-var videos=[{id:3,name:"islamic",uid:12},{id:2,name:"music",uid:11},{id:1,name:"documentary",uid:10}];
+var videos = [{
+    id: 3,
+    name: "islamic",
+    uid: 12
+}, {
+    id: 2,
+    name: "music",
+    uid: 11
+}, {
+    id: 1,
+    name: "documentary",
+    uid: 10
+}];
 
-var reported=[{uid:10,vid:3},{uid:10,vid:1},{uid:12,vid:1}];
+var reported = [{
+    uid: 10,
+    vid: 3
+}, {
+    uid: 10,
+    vid: 1
+}, {
+    uid: 12,
+    vid: 1
+}];
 
-var users=[{id:1,name:"aman"},{id:2,name:"farooq"},{id:3,name:"usama"}];
+var users = [{
+    id: 11,
+    name: "aman"
+}, {
+    id: 12,
+    name: "farooq"
+}, {
+    id: 10,
+    name: "usama"
+}];
 
 
 //initial data
@@ -28,7 +57,7 @@ console.log("\n-------------------------------------------------------\n");
 console.log("maping an array of Ids from members of {id,name}\n");
 var res;
 
-res=videos.map(function (params) {
+res = videos.map(function (params) {
     return params.id;
 });
 console.log(res);
@@ -36,9 +65,18 @@ console.log(res);
 console.log("\n-------------------------------------------------------\n");
 
 
-//extratct reported video by each users
+//showing videos that are not reported by user 10
+console.log("showing videos that are not reported by user 10\n");
 
 
+res = lodash.differenceWith(videos, reported.filter((val) => {
+    return val.uid == 10
+}), (vid, repo) => {
+    return vid.id == repo.vid
+});
+console.log(res);
+
+console.log("\n-------------------------------------------------------\n");
 
 
 //extract all videos which are not reported by users
